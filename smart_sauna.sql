adb4597e6a29ec02e6da469d24ddc9a0f6fbf9a2
d.cioffo <d.cioffo@studenti.unipi.it>
2021-08-17 15:23:18 +0200

    database file included

---
+++ smart_sauna.sql
@@ -0,0 +1,131 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Ago 17, 2021 alle 15:19
+-- Versione del server: 10.1.25-MariaDB
+-- Versione PHP: 7.1.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `smart_sauna`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `air_quality`
+--
+
+CREATE TABLE `air_quality` (
+  `id` int(11) NOT NULL,
+  `node` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `concentration` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `humidity`
+--
+
+CREATE TABLE `humidity` (
+  `id` int(11) NOT NULL,
+  `node` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `percentage` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `presence`
+--
+
+CREATE TABLE `presence` (
+  `id` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `temperature`
+--
+
+CREATE TABLE `temperature` (
+  `id` int(11) NOT NULL,
+  `node` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `degrees` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `air_quality`
+--
+ALTER TABLE `air_quality`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `humidity`
+--
+ALTER TABLE `humidity`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `presence`
+--
+ALTER TABLE `presence`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `temperature`
+--
+ALTER TABLE `temperature`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `air_quality`
+--
+ALTER TABLE `air_quality`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT per la tabella `humidity`
+--
+ALTER TABLE `humidity`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT per la tabella `presence`
+--
+ALTER TABLE `presence`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT per la tabella `temperature`
+--
+ALTER TABLE `temperature`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
