ca0280ba2a2a72ede803af17714e3027c5aea714
osboxes <d.cioffo@studenti.unipi.it>
2021-08-18 09:22:29 -0400

    Presence sensor added

---
+++ CoAP-network/presence/presence_CoAP-server.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "contiki.h"
+#include "sys/etimer.h"
+#include "coap-engine.h"
+
+#include "node-id.h"
+#include "net/ipv6/simple-udp.h"
+#include "net/ipv6/uip.h"
+#include "net/ipv6/uip-ds6.h"
+#include "net/ipv6/uip-debug.h"
+#include "routing/routing.h"
+
+#include "sys/log.h"
+#define LOG_MODULE "App"
+#define LOG_LEVEL LOG_LEVEL_APP
+
+#define INTERVAL_BETWEEN_SIMULATIONS 3
+#define INTERVAL_BETWEEN_CONNECTION_TESTS 1
+
+extern coap_resource_t res_presence;
+static struct etimer simulation_timer;
+static struct etimer connectivity_timer;
+
+PROCESS(presence_server, "Presence Server");
+AUTOSTART_PROCESSES(&presence_server);
+
+static bool is_connected()
+{
+	if(NETSTACK_ROUTING.node_is_reachable())
+	{
+		LOG_INFO("The Border Router is reachable\n");
+		return true;
+  	}
+	else
+	{
+		LOG_INFO("Waiting for connection with the Border Router\n");
+	}
+	return false;
+}
+
+PROCESS_THREAD(presence_server, ev, data)
+{
+	PROCESS_BEGIN();
+	PROCESS_PAUSE();
+
+	LOG_INFO("Starting Presence CoAP-Server\n");
+	coap_activate_resource(&res_presence, "presence");
+
+	// try to connect to the border router
+	etimer_set(&connectivity_timer, CLOCK_SECOND * INTERVAL_BETWEEN_CONNECTION_TESTS);
+	PROCESS_WAIT_UNTIL(etimer_expired(&connectivity_timer));
+	while(!is_connected())
+	{
+		etimer_reset(&connectivity_timer);
+		PROCESS_WAIT_UNTIL(etimer_expired(&connectivity_timer));
+	}
+
+	etimer_set(&simulation_timer, CLOCK_SECOND * INTERVAL_BETWEEN_SIMULATIONS); // every three seconds
+	while(1) 
+	{
+		PROCESS_WAIT_EVENT();
+		if(ev == PROCESS_EVENT_TIMER && data == &simulation_timer)
+		{
+			res_presence.trigger();	
+			etimer_set(&simulation_timer, CLOCK_SECOND * INTERVAL_BETWEEN_SIMULATIONS);
+		}
+	}                             
+	PROCESS_END();
+}
---
+++ presence/resources/res-presence.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "coap-engine.h"
+#include "sys/log.h"
+#include <time.h>
+
+#define LOG_MODULE "App"
+#define LOG_LEVEL LOG_LEVEL_APP
+
+static void presence_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+static void presence_event_handler(void);
+static void presence_put_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+
+EVENT_RESOURCE(res_presence,
+	"title=\"Presence sensor\";obs",
+	presence_get_handler,
+        NULL,
+        presence_put_handler,
+        NULL, 
+	presence_event_handler);
+
+static unsigned int number_of_people = 0;
+static unsigned int max_number_of_people = 20;
+
+static bool update_counter () // simulate the behavior of the real sensor
+{
+	bool updated = false;
+	srand(time(NULL));
+	int value = rand() % 10; // a number between 0 and 9
+	if ((value%2) == 0) // if the value is even try to increase
+	{
+		if (number_of_people < max_number_of_people)
+		{
+			number_of_people = number_of_people + 1;
+			updated = true;
+		}
+	}
+	else // try to decrease
+	{
+		if (number_of_people > 0)
+		{
+			number_of_people = number_of_people - 1;
+			updated = true;	
+		}
+	}
+	return updated;
+}
+
+static void presence_event_handler(void)
+{
+	if (update_counter()) // if the value is changed
+	{
+		LOG_INFO("Number of people inside the sauna: %u\n", number_of_people);
+		// Notify all the observers
+    		coap_notify_observers(&res_presence);	
+	}
+}
+
+static void presence_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+  	coap_set_header_content_format(response, TEXT_PLAIN);
+  	coap_set_payload(response, buffer, snprintf((char *)buffer, preferred_size, "%u", (unsigned int) number_of_people));
+}
+
+static void presence_put_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+	size_t len = 0;
+    	const uint8_t* payload = NULL;
+  	if((len = coap_get_payload(request, &payload))) 
+	{
+		int new_value = atoi((char*)payload); // new_value >= 0 for constraint
+		LOG_INFO("The maximum number of people allowed is now: %d\n", new_value);
+		max_number_of_people = (unsigned int) new_value;
+	}
+	else
+	{
+		coap_set_status_code(response, BAD_REQUEST_4_00);  
+	}
+}
